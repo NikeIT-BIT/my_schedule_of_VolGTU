8ad9aae4170960d2ee454f9aec800fd7bd40b777
Kovalev Ivan <vania8022@gmail.com>
2023-07-26 03:12:25 +0300

    ИИсправил ошибку, позволяющую затирать одно занятие при функции обмена занятий

--- MyShedule/MainForm.cs
+++ MyShedule/MainForm.cs
@@ -12,6 +12,8 @@ using Yogesh.Extensions;
 using MyShedule.SheduleClasses;
 using MySql.Data.MySqlClient;
 using SD = System.Data;
+using Org.BouncyCastle.Crypto.Engines;
+using DocumentFormat.OpenXml.Drawing.Charts;
 
 namespace MyShedule
 {
@@ -523,7 +525,13 @@ namespace MyShedule
         public SheduleTime T2;
         public SheduleTime T3;
         public SheduleTime T4;
-
+        private int coloumnIndLs1;
+        private int rowIndLs1;
+        private int coloumnIndLs2;
+        private int rowIndLs2;
+        private DataGridViewCell c1;
+        private DataGridViewCell c2;
+        private bool isFirstTime = true;
         private void button1_Click(object sender, EventArgs e)
         {
             NumSelectLesson = 1;
@@ -536,6 +544,8 @@ namespace MyShedule
 
         private void ExchangeLessons(SheduleLesson item1, SheduleLesson item2, SheduleTime time1, SheduleTime time2)
         {
+            if(c1 == c2 && isFirstTime)
+            { return; }
             if (item1 != null && item2 != null)
             {
                 SheduleLesson tmp1 = item1.Copy();
@@ -543,9 +553,16 @@ namespace MyShedule
                 item1.Dates = item2.Dates;
                 item2.Time = tmp1.Time.Copy();
                 item2.Dates = tmp1.Dates;
-
+                DataGridViewCell c3;
                 Shedule.GetLesson(time1, item2.Room).UpdateFields(item2.Teacher, item2.Discipline, item2.Groups, item2.Type, item2.Dates);
                 Shedule.GetLesson(time2, item1.Room).UpdateFields(item1.Teacher, item1.Discipline, item1.Groups, item1.Type, item1.Dates);
+                if(isFirstTime)
+                {
+                    c3 = c1;
+                    c1 = c2;
+                    c2 = c3;
+                }
+
             }
             else if (!(time1 is null) && !(time2 is null))
             {
@@ -553,22 +570,58 @@ namespace MyShedule
                 {
                     SheduleLesson lsn = Shedule.GetLesson(time1, item2.Room).Copy();
                     Shedule.GetLesson(time1, item2.Room).UpdateFields(item2.Teacher, item2.Discipline, item2.Groups, item2.Type, lsn.Dates);
+                    if(isFirstTime) c2 = c1;
+                    
                     item2.Clear();
                 }
                 else if (item1 != null && item2 == null)
                 {
                     SheduleLesson lsn = Shedule.GetLesson(time2, item1.Room).Copy();
                     Shedule.GetLesson(time2, item1.Room).UpdateFields(item1.Teacher, item1.Discipline, item1.Groups, item1.Type, lsn.Dates);
+                    if (isFirstTime) c1 = c2;
                     item1.Clear();
                 }
+                isFirstTime = false;
             }
         }
 
         private void btnExchangeLessons_Click(object sender, EventArgs e)
         {
-            ExchangeLessons(L1, L3, T1, T3);
-            ExchangeLessons(L2, L4, T2, T4);
-            UpdateTableShedule();
+            try
+            {
+                isFirstTime = true;
+                c1 = dgvShedule.Rows[rowIndLs1].Cells[coloumnIndLs1];
+                c2 = dgvShedule.Rows[rowIndLs2].Cells[coloumnIndLs2];
+
+                ExchangeLessons(L1, L3, T1, T3);
+                if (c1 == c2)
+                {
+                    rowIndLs1 = rowIndLs2;
+                    coloumnIndLs1 = coloumnIndLs2;
+                }
+                else 
+                {
+                    int bufferIndex = rowIndLs1;
+                    rowIndLs1 = rowIndLs2;
+                    rowIndLs2 = bufferIndex;
+                    bufferIndex = coloumnIndLs1;
+                    coloumnIndLs1 = coloumnIndLs2;
+                    coloumnIndLs2 = bufferIndex;
+                }
+                ExchangeLessons(L2, L4, T2, T4);
+
+                UpdateTableShedule();
+                c1 = dgvShedule.Rows[rowIndLs1].Cells[coloumnIndLs1];
+                c2 = dgvShedule.Rows[rowIndLs2].Cells[coloumnIndLs2];
+                NumSelectLesson = 1;
+                ViewSelectLesson(c1);
+                NumSelectLesson = 2;
+                ViewSelectLesson(c2);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Выберете две ячейки, которые вы хотели бы поменять.");
+            }
         }
 
         private void SetDataSelectLessonForm(SheduleLesson item1, SheduleLesson item2, Label txtTeacher, Label txtDiscipline,
@@ -600,7 +653,8 @@ namespace MyShedule
             if (NumSelectLesson == 1)
             {
                 SetDataSelectLessonForm(lesson1, lesson2, txtTeacher1, txtDiscipline1, txtRoom1, txtTypeLesson1);
-
+                coloumnIndLs1 = cell.ColumnIndex;
+                rowIndLs1 = cell.RowIndex;
                 L1 = lesson1;
                 L2 = lesson2;
                 T1 = Tag.Time1.Copy();
@@ -609,7 +663,8 @@ namespace MyShedule
             else if (NumSelectLesson == 2)
             {
                 SetDataSelectLessonForm(lesson1, lesson2, txtTeacher2, txtDiscipline2, txtRoom2, txtTypeLesson2);
-
+                coloumnIndLs2 = cell.ColumnIndex;
+                rowIndLs2 = cell.RowIndex;
                 L3 = lesson1;
                 L4 = lesson2;
                 T3 = Tag.Time1.Copy();
@@ -715,7 +770,6 @@ namespace MyShedule
             string fileName;
             try
             {
-                
                 SaveFileDialog newExcelFile = new SaveFileDialog();
                 newExcelFile.Filter = "xls files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
                 newExcelFile.Title = "Экспортировать в эксель";
@@ -730,13 +784,11 @@ namespace MyShedule
                     {
                         worksheet.Cell(1, indexColoumn + 1).Value = dgvShedule.Columns[indexColoumn].HeaderText;
                     }
-
                     for (int i = 0; i < dgvShedule.Rows.Count; i++)
                     {
                         for (int j = 0; j < dgvShedule.Columns.Count; j++)
                         {
                             worksheet.Cell(i + 2, j + 1).Value = dgvShedule.Rows[i].Cells[j].Value.ToString();
-
                             if (worksheet.Cell(i + 2, j + 1).Value.ToString().Length > 0)
                             {
                                 XLAlignmentHorizontalValues align;
@@ -762,20 +814,15 @@ namespace MyShedule
                                     case DataGridViewContentAlignment.TopCenter:
                                         align = XLAlignmentHorizontalValues.Center;
                                         break;
-
                                     default:
                                         align = XLAlignmentHorizontalValues.Left;
                                         break;
                                 }
-
                                 worksheet.Cell(i + 2, j + 1).Style.Alignment.Horizontal = align;
-
                                 XLColor xlColor = XLColor.FromColor(dgvShedule.Rows[i].Cells[j].Style.SelectionBackColor);
                                 worksheet.Cell(i + 2, j + 1).AddConditionalFormat().WhenLessThan(1).Fill.SetBackgroundColor(xlColor);
-
                                 worksheet.Cell(i + 2, j + 1).Style.Font.FontName = dgvShedule.Font.Name;
                                 worksheet.Cell(i + 2, j + 1).Style.Font.FontSize = dgvShedule.Font.Size;
-
                             }
                         }
                     }
